53e0f46cce52a4547348f1983d577de401a97bee
Qaisar-Muhammad <qaisardeveloper84@gmail.com>
2026-02-16 12:11:51 +0500

    changes

---
+++ hel.py
@@ -0,0 +1,3 @@
+print('i am Qaisar muhammad khan and i am studing in uet peshawar and my department computer science and now i am studing data science and artificail intelligence and etc so on ')
+print(45)
+print('cs')
